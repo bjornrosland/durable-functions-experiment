fe7d3a9b599403aecf5f0e9a619f094d9024674b
bjoernrosland <bjornr@omega365.com>
2021-09-10 10:56:26 +0200

    using blob queue function

--- src/DurableFunctions/DurableFunctions/BlobQueue.cs
+++ src/DurableFunctions/DurableFunctions/BlobQueue.cs
@@ -1,16 +1,37 @@
 using System;
+using System.Threading.Tasks;
+using DurableFunctions.Models;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace DurableFunctions
 {
     public static class BlobQueue
     {
         [FunctionName("BlobQueue")]
-        public static void Run([QueueTrigger("test-queue", Connection = "BlobQueue")]string myQueueItem, ILogger log)
+        public static async Task RunAsync([QueueTrigger("test-queue", Connection = "BlobQueue")]string myQueueItem,
+            [DurableClient] IDurableOrchestrationClient starter,
+            ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+            var message = JsonConvert.DeserializeObject<QueueMessageDto>(myQueueItem);
+            string instanceId = await starter.StartNewAsync("BatchOrchestrator", message);
+            log.LogInformation($"Started orchestration with ID = '{instanceId}'.");
         }
+
+        [FunctionName("BatchOrchestrator")]
+        public static async Task RunOrchestrator(
+        [OrchestrationTrigger] IDurableOrchestrationContext context,
+        ILogger log)
+        {
+            QueueMessageDto message = context.GetInput<QueueMessageDto>();
+            string messageContent = JsonConvert.SerializeObject(message);
+            log.LogInformation(messageContent);
+
+        }
+
+
     }
 }
---
+++ src/DurableFunctions/DurableFunctions/Models/QueueMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DurableFunctions.Models
+{
+    public class QueueMessageDto
+    {
+        public string JobId { get; set; }
+        public string[] Files { get; set; }
+    }
+}
